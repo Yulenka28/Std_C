8436aa45cf3aff41bbd612011bbcbafd7378930a
Yulia <julsik28@gmail.com>
2023-07-02 00:07:13 +0500

    feat:add zad4

---
+++ Examples/Lection4/dvumer_massiv/Program.cs
@@ -0,0 +1,25 @@
+﻿// создание строкового и чисельного двумерного массива
+
+string[,] table = new string [2, 5];
+
+table[1, 2] = "word";
+
+for (int rows = 0; rows < 2; rows++)
+{
+    for (int columns = 0; columns < 5; columns++)
+    {
+        Console.Write($"--{table[rows, columns]}--");
+    }
+    Console.WriteLine();
+}
+
+int[,] matrix = new int[3, 4];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write($"{matrix[i, j]} ");
+    }
+    Console.WriteLine();
+}
