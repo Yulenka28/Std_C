f1018f89e226169c2f3bb20ec2e6d3d34e7cd800
Yulia <julsik28@gmail.com>
2023-07-02 23:54:00 +0500

    feat:lection4 fibonacci

---
+++ Examples/Lection4/fibonacci_recursia/Program.cs
@@ -0,0 +1,10 @@
+﻿// Посчитаем число Фибоначи от первых 20 чисел, рекурсия
+double Fibonacci(int n)
+{
+ if(n == 1 || n == 2) return 1;
+ else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+for (int i = 1; i < 20; i++)
+{
+ Console.WriteLine(Fibonacci(i));
+}
